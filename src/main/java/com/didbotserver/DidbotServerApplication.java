3d737f7cb3dbe45a3b4c0cdf33ae73f0353f0592
Yonghwi Kim <yh0921k@gmail.com>
2021-11-10 15:07:46 +0900

    init: Discord Bot 구현을 위한 프로젝트 초기화

---
+++ src/main/java/com/didbotserver/DidbotServerApplication.java
@@ -0,0 +1,12 @@
+package com.didbotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidbotServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DidbotServerApplication.class, args);
+  }
+}
---
+++ src/test/java/com/didbotserver/DidbotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didbotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidbotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
